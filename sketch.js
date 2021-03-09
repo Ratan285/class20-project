42d9802f2089b93369d0fd1822a20b4576fb2992
Ratan285 <76036908+Ratan285@users.noreply.github.com>
2021-03-10 00:21:27 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,25 +1,46 @@
 var backGround,backgroundImage;
 var cat,catImage,mouse,mouseImage;
+var catRunningimage;
 
 
 
 
 
 function preload(){
-createCanvas(1000,700);
+
 backgroundImage=loadImage("garden.png");
 catImage=loadImage("tomOne.png");
 mouseImage=loadImage("jerryOne.png");
+catRunningimage = loadImage("tomTwo.png");
 }
 function setup(){
-backGround=createSprite(0,50,1000,700);
-//backGround.addImage(backgroundImage);
+    createCanvas(1000,700);
 
-cat = createSprite(100,330,20,20);
-cat.addImage(catImage);
-cat.scale=0.1;
+    backGround=createSprite(500,350,1000,700);
+    backGround.addImage(backgroundImage);
+    backGround.scale = 1.03;
+
+    cat = createSprite(500,630,20,20);
+    cat.addImage(catImage);
+    cat.scale=0.1;
+
+    mouse = createSprite(150,630,20,20);
+    mouse.addImage(mouseImage);
+    mouse.scale=0.125;
+
+    keypressed();
+
 }
+
 function draw(){
 background("red");
 drawSprites();
 }
+ function keypressed(){
+     if(keyCode===LEFT_ARROW){
+         cat.velocityX=-5;
+         cat.addAnimation(catRunningimage,"catRunning");
+         cat.changeAnimation("catRunning");
+         
+     }
+ }
